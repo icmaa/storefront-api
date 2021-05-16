6199cb243a9036e2248a0dfbbf0085e8d35086be
Christian Ewald <c.ewald@impericon.com>
2021-05-16 21:20:48 +0200

    #225953 Add support for Google App Engine (#33)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { Server } from '@storefront-api/core'
 import modules from './modules'
 
-// Add custom datadog tracer
-import 'icmaa-monitoring/datadog/dd-trace'
-
 const server = new Server({
   modules
 })
--- src/modules/icmaa/index.ts
+++ src/modules/icmaa/index.ts
@@ -1,6 +1,7 @@
 import { StorefrontApiModule, registerExtensions } from '@storefront-api/lib/module'
 import { StorefrontApiContext } from '@storefront-api/lib/module/types'
-import path from 'path'
+import invalidate from './invalidate'
+import * as path from 'path'
 
 export const IcmaaModule: StorefrontApiModule = new StorefrontApiModule({
   key: 'icmaa-module',
@@ -9,5 +10,8 @@ export const IcmaaModule: StorefrontApiModule = new StorefrontApiModule({
     const rootPath = path.join(__dirname, '..')
     const registeredExtensions: string[] = config.get('modules.icmaa.registeredExtensions')
     registerExtensions({ app, config, db, registeredExtensions, rootPath })
+
+    app.get('/api/invalidate/all', invalidate)
+    app.get('/_ah/warmup', (req, res) => res.send('Warm-Up for storefront-api'))
   }
 })
