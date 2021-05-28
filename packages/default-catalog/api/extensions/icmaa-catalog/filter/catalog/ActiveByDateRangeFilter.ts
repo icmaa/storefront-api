54ca338828b44c2db88b8b006612a5cef24e8b79
cewald <c.ewald@impericon.com>
2021-05-28 10:21:25 +0000

    Improve code

--- packages/default-catalog/api/extensions/icmaa-catalog/filter/catalog/ActiveByDateRangeFilter.ts
+++ packages/default-catalog/api/extensions/icmaa-catalog/filter/catalog/ActiveByDateRangeFilter.ts
@@ -3,7 +3,7 @@ import { FilterInterface } from 'storefront-query-builder'
 import moment from 'moment-timezone'
 import omit from 'lodash/omit'
 
-interface FilterDefaults { from: string, to: string, utcOffset: string }
+interface FilterDefaults { from: string, to: string, timezone?: string }
 const defaultFields = config.get<FilterDefaults>('extensions.icmaa-catalog.defaultDateRangeFields')
 
 const filter: FilterInterface = {
@@ -15,15 +15,24 @@ const filter: FilterInterface = {
       value = {}
     }
 
-    let dateTime = value.dateTime || defaultFields.utcOffset
+    let dateTime = value.dateTime || 'now'
+    const timezone = defaultFields.timezone || false
     const fromField = value.fromField || defaultFields.from
     const toField = value.toField || defaultFields.to
 
-    // Get UTC offset from timezone using the dateTime parameter.
-    // We assume that times in the ES are always saved in UTC format so we can use the offset of the timezone.
-    if (!dateTime.startsWith('now') && !moment(new Date(dateTime), moment.ISO_8601).isValid()) {
-      const tzOffset = moment.tz(dateTime).utcOffset() / 60
-      dateTime = Math.sign(tzOffset) !== -1 ? `now+${tzOffset}h` : `now${tzOffset}h`
+    // Get UTC offset or time using timezone.
+    // We assume that times in the ES are always saved in UTC – if not we can set an offset
+    // by the desired timezone or format the transmitted UTC date into the target timezone.
+    if (timezone) {
+      const isValidDateTime = moment(new Date(dateTime), moment.ISO_8601).isValid()
+      if (dateTime === 'now') {
+        const tzOffset = moment.tz(timezone).utcOffset() / 60
+        dateTime = Math.sign(tzOffset) !== -1 ? `now+${tzOffset}h` : `now${tzOffset}h`
+      } else if (isValidDateTime) {
+        dateTime = isValidDateTime
+          ? moment.tz(dateTime, timezone).format('yyyy-MM-DD HH:mm:ss')
+          : moment.tz(timezone).format('yyyy-MM-DD HH:mm:ss')
+      }
     }
 
     value = omit(value, ['dateTime', 'fromField', 'toField'])
