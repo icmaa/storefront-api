a90a6b93f529cbbb00914362a77fb144fa55458b
cewald <c.ewald@impericon.com>
2021-06-21 10:34:26 +0000

    Revert "Apm list endpoint"
    This reverts commit 09fe03af707c4acd8639f26b6c6a081e0928ee77.

--- src/modules/icmaa-checkout-com/index.ts
+++ src/modules/icmaa-checkout-com/index.ts
@@ -6,9 +6,10 @@ import { newMagentoClientAction } from 'icmaa/helpers'
 module.exports = ({ config }: ExtensionAPIFunctionParameter): Router => {
   const api = Router()
 
-  api.get('/apm-list', async (req, res) => {
-    const client = newMagentoClientAction('checkoutcom', 'apmlist', 'checkoutcom/', config, req)
-    client.checkoutcom.apmlist(req.body)
+  api.post('/payment-details', async (req, res) => {
+    const client = newMagentoClientAction('checkoutcom', 'details', 'checkoutcom/', config, req)
+    console.log(req.body)
+    client.checkoutcom.details(req.body)
       .then((result) => {
         apiStatus(res, result, 200)
       }).catch(err => {
