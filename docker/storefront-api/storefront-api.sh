0856c653f8e46c68fab17cc566e221e75d83f31b
Christian Ewald <c.ewald@impericon.com>
2021-09-09 11:47:47 +0000

    Update

--- docker/storefront-api/storefront-api.sh
+++ docker/storefront-api/storefront-api.sh
@@ -3,6 +3,10 @@ set -e
 
 yarn install || exit $?
 
+if [ ! -f "./config/custom-environment-variables.json" ]; then
+  config-sync -r storefront-api -d ./
+fi
+
 if [ "$NODE_CONFIG_ENV" = 'development' ]; then
   yarn build:clean
   yarn dev
