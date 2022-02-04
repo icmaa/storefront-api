b4cff0b56f1164ee85f4ec7e8e94c8d27bfb4fda
Christian Ewald <c.ewald@impericon.com>
2022-02-04 13:36:51 +0100

    Bugfixes and improvements (#57)
    * Bugfix for `loadCustomFilters` in catalog-requests to prevent that the original cache-object gets overwritten
    * Update `sharp` library as the current version will fail in GAE deployment
    * Remove old SC build job from Github-Actions
    * Remove `DefaultImgModule` as we don't use it

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,7 +2,6 @@ import 'icmaa-logs/lib/gcloud'
 
 import { DefaultVuestorefrontApiModule } from '@storefront-api/default-vsf'
 import { DefaultCatalogModule } from '@storefront-api/default-catalog'
-import { DefaultImgModule } from '@storefront-api/default-img'
 import { IcmaaModule, IcmaaMiddleware } from 'icmaa'
 import { StorefrontApiModule } from '@storefront-api/lib/module'
 import * as magento1 from 'icmaa-platform-magento1'
@@ -16,7 +15,6 @@ export const modules: StorefrontApiModule[] = [
     }
   }),
   DefaultCatalogModule(),
-  DefaultImgModule(),
   IcmaaModule
 ]
 
