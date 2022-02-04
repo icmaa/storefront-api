d0069ab518e376fd6180d2a4a266fa5e50f1088f
Christian Ewald <c.ewald@impericon.com>
2022-02-04 06:55:12 +0000

    Add FPC caching to new URL mapper

--- src/modules/icmaa/url.ts
+++ src/modules/icmaa/url.ts
@@ -1,10 +1,13 @@
 import { ExtensionAPIFunctionParameter } from '@storefront-api/lib/module'
-import { getClient as getElasticClient } from '@storefront-api/lib/elastic'
+import { getClient as getElasticClient, getHits } from '@storefront-api/lib/elastic'
 import { apiStatus, getCurrentStoreView, getCurrentStoreCode } from '@storefront-api/lib/util'
+import cache from '@storefront-api/lib/cache-instance'
+import Logger from '@storefront-api/lib/logger'
 import loadCustomFilters from '@storefront-api/default-catalog/helper/loadCustomFilters'
 import ProcessorFactory from '@storefront-api/default-catalog/processor/factory'
 
 import { Router } from 'express'
+import { IConfig } from 'config'
 import { elasticsearch, SearchQuery, ElasticsearchQueryConfig } from 'storefront-query-builder'
 import bodybuilder from 'bodybuilder'
 import get from 'lodash/get'
@@ -47,10 +50,45 @@ const checkFieldValueEquality = ({ config, result, value }) => {
 export default ({ config }: ExtensionAPIFunctionParameter): Router => {
   const router = Router()
 
+  async function _cacheStorageHandler (config: IConfig, result: Record<string, any>, hash: string, tags = []): Promise<void> {
+    if (config.get<boolean>('server.useOutputCache') && cache) {
+      return cache.set(
+        'api:' + hash,
+        result,
+        tags
+      ).catch((err) => {
+        Logger.error(err)
+      })
+    }
+  }
+
   router.post('/map', async (req, res) => {
+    const s = Date.now()
     const { url, excludeFields, includeFields } = req.body
     if (!url) {
       return apiStatus(res, 'Missing url', 500)
+    }
+
+    if (config.get<boolean>('server.useOutputCache') && cache) {
+      const isCached = await cache.get('api:' + url)
+        .then(output => {
+          if (output !== null) {
+            res.setHeader('x-vs-cache', 'hit')
+            Logger.debug(`Cache hit [${req.url}], cached request: ${Date.now() - s}ms`)
+            res.json(output)
+            return true
+          } else {
+            res.setHeader('x-vs-cache', 'miss')
+            Logger.debug(`Cache miss [${req.url}], request: ${Date.now() - s}ms`)
+            return false
+          }
+        })
+        .catch(err => {
+          Logger.error(err)
+          return false
+        })
+
+      if (isCached) return
     }
 
     const indexName = getCurrentStoreView(getCurrentStoreCode(req)).elasticsearch.index
@@ -65,7 +103,7 @@ export default ({ config }: ExtensionAPIFunctionParameter): Router => {
 
     try {
       const esResponse = await getElasticClient(config).search(esQuery)
-      let result = get(esResponse, 'body.hits.hits[0]', null)
+      let result = getHits(esResponse)[0]
 
       if (result && checkFieldValueEquality({ config, result, value: url })) {
         result = addResultTypeByIndexName({ result, indexName })
@@ -76,10 +114,15 @@ export default ({ config }: ExtensionAPIFunctionParameter): Router => {
           resultProcessor = factory.getAdapter('default', indexName, req, res)
         }
 
+        const tagsArray = [result._type]
+        const tagPrefix = result._type[0].toUpperCase()
+        tagsArray.push(`${tagPrefix}${result?._id}`)
+
         resultProcessor
           .process(esResponse.body.hits.hits, null)
-          .then(pResult => {
+          .then(async pResult => {
             pResult = pResult.map(h => Object.assign(h, { _score: h._score }))
+            await _cacheStorageHandler(config, pResult[0], url, tagsArray)
             return res.json(pResult[0])
           }).catch((err) => {
             console.error(err)
